275f3c60dab4e036bd254b0bc89e7c52912f8dae
Nishok <nishoklibin@gmail.com>
2025-10-23 19:56:59 +0530

    New files

---
+++ Tuple Task Using Loop/Index Display - Print index and value of each element in (100, 200, 300, 400) using a loop..py
@@ -0,0 +1,11 @@
+###Index Display - Print index and value of each element in (100, 200, 300, 400) using a loop.
+
+numbers = (100, 200, 300, 400)
+
+for index, value in enumerate(numbers):
+    print("Index:", index, "Value:", value)
+
+
+
+
+
